e7b89c8dcf8d5ad16c9fb613c01ec41da0c6738d
D. Vespa <d.vespa@gmail.com>
2018-08-11 16:28:27 -0300

    Meta tags and example menu added

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
+import Menu from '../components/Menu';
 
 export default class MyDocument extends Document {
   render() {
@@ -7,8 +8,12 @@ export default class MyDocument extends Document {
       <html lang="en">
         <Head>
           <link rel="stylesheet" href="/_next/static/style.css" />
+          <link rel="shortcut icon" type="image/x-icon" href="/static/assets/favicon.ico" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>Opportunities</title>
         </Head>
         <body>
+          <Menu />
           <Main />
           <NextScript />
         </body>
