b140c63500014a89b8a9e4c64342368253cca215
D. Vespa <d.vespa@gmail.com>
2018-08-11 20:29:24 -0300

    Updating tests

--- components/Form/Input/index.test.jsx
+++ components/Form/Input/index.test.jsx
@@ -4,8 +4,15 @@ import Input from './index';
 
 describe('[Component: Input]', () => {
   let wrapper;
+  const props = {
+    title: 'Name',
+    name: 'name',
+    type: 'text',
+    required: true,
+    validation: 'no-empty',
+  };
   beforeEach(() => {
-    wrapper = shallow(<Input />);
+    wrapper = shallow(<Input {...props} />);
   });
   it('should match snapshot', () => {
     expect(wrapper).toMatchSnapshot();
--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,12 @@ module.exports = {
   setupFiles: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   testURL: 'http://localhost',
+  "moduleNameMapper":{
+    "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
+    "\\.(gif|ttf|eot|svg)$": "<rootDir>/__mocks__/fileMock.js"
+  },
+  "collectCoverage": true,
+  "collectCoverageFrom": [
+    "**/components/**.{js,jsx}",
+  ],
 }
