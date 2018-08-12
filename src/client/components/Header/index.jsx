a5bab0648385fa424ec5f9ac1a022ed6c7346f4b
D. Vespa <d.vespa@gmail.com>
2018-08-12 20:14:43 -0300

    Adding Header component

--- src/client/components/Header/index.jsx
+++ src/client/components/Header/index.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import css from './style.scss';
 
 const Index = () => (
-  <header className={css.header__opening}>
-    oij
+  <header className={css.header}>
+    <div className={css.header__opening}>
+      <div className={`${css['header__opening__box--opacity']} ${css.header__opening__box} `} />
+      <div className={css.header__opening__box}>
+        <h1>Purchasing assistant</h1>
+        <h2>Amsterdam, the Netherlands</h2>
+      </div>
+    </div>
   </header>
 );
 
